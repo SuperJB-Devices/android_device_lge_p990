f99e971660c3a24742ea98ee8b6def1ceee52be5
podxboq <podxboq@gmail.com>
2011-09-23 18:25:28 +0200

    New kernel config file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,10 @@
 # inherit from the proprietary version
 -include device/lge/star-common/BoardConfigCommon.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := p990
+#Kernel config
+include device/lge/p990/kernel.mk
 
-BOARD_KERNEL_CMDLINE := mem=383M@0M nvmem=128M@384M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990
-BOARD_KERNEL_BASE := 0x10000000
+TARGET_BOOTLOADER_BOARD_NAME := p990
 BOARD_PAGE_SIZE := 0x00000800
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00800000
@@ -12,21 +12,6 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x15cb2c00
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x5e7dc000
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Build kernel
-TARGET_CUSTOM_KERNEL := true
-#TARGET_PREBUILT_KERNEL := device/lge/p990/kernel
-#PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=device/lge/p990/kernel
-PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel/tegra
-PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=superteam_p990_defconfig
-PRODUCT_SPECIFIC_DEFINES += TARGET_NO_BUILD_WIFI=true
-    
-# Copy kernel file
-ifneq ($(TARGET_PREBUILT_KERNEL),)
-	PRODUCT_COPY_FILE += \
-		$(TARGET_PREBUILT_KERNEL):kernel
-endif
-
 
 BOARD_VOLD_MAX_PARTITIONS := 9
 
