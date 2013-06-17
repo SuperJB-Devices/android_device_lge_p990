e04ea273d5eee0d2c7fb36a74c6f99db38ad25c3
podxboq <podxboq@gmail.com>
2013-06-17 23:52:47 +0200

    Fork jellybean version from CyanogenMod

--- bbversion/Android.mk
+++ bbversion/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The CyanogenMod Project
+# Copyright (C) 2012 The CyanogenMod project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,23 +13,15 @@
 # limitations under the License.
 
 
-ifneq ($(filter p990 p999, $(TARGET_BOOTLOADER_BOARD_NAME)),)
-
 LOCAL_PATH:= $(call my-dir)
-# HAL module implemenation, not prelinked and stored in
-# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := eng
 
-LOCAL_SRC_FILES := lights.c
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := cpversion.c
 
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE := lgcpversion
 
-LOCAL_SHARED_LIBRARIES := liblog
-
-LOCAL_MODULE := lights.$(TARGET_BOOTLOADER_BOARD_NAME)
-
-include $(BUILD_SHARED_LIBRARY)
-
-endif
+include $(BUILD_EXECUTABLE)
---
+++ p990.mk
@@ -0,0 +1,31 @@
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu.mk)
+
+$(call inherit-product, device/lge/star-common/star.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/p990/overlay
+
+# Inherit non-open-source blobs.
+$(call inherit-product-if-exists, vendor/lge/p990/p990-vendor.mk)
+
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    device/lge/p990/init.p990.rc:root/init.star.rc \
+    $(LOCAL_PATH)/ueventd.tegra.rc:root/ueventd.star.rc \
+    $(LOCAL_PATH)/fstab.p990:root/fstab.p990
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/init.vsnet:system/bin/init.vsnet \
+    $(LOCAL_PATH)/init.vsnet-down:system/bin/init.vsnet-down \
+    $(LOCAL_PATH)/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/wireless.ko:system/lib/modules/wireless.ko
+
+PRODUCT_PACKAGES += \
+    lgcpversion
+
+PRODUCT_NAME := full_p990
+PRODUCT_DEVICE := p990
+PRODUCT_MODEL := LG-P990
